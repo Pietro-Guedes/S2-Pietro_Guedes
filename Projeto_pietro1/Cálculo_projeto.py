9e002c1eed8d4fda1e5ee2f1a8e24da4a5c99499
Pietro-Guedes <pietro.g.oliveira6@aluno.senai.br>
2025-04-08 08:46:23 -0300

    Cálculo_projeto.py

--- Projeto_pietro1/Cálculo_projeto.py
+++ Projeto_pietro1/Cálculo_projeto.py
@@ -6,16 +6,16 @@ def operacao():
     global num, num2, var
     if var == '+':
         num3 = num + num2
-        
+        print(f'O resultado é {num3}')
     elif var == '-':
         num4 = num - num2
-        
+        print(f'O reseltado é {num4}')
     elif var == '*':
         num5 = num * num2
-        
+        print(f'O resultado é {num5}')
     elif var == '/':
         num6 = num / num2
-        
+        print('O resultado é {num6}')
     else:
-       
+        print('Erro')
 operacao()
