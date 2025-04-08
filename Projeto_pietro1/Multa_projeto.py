c60d5aa25b00d505d9ec99809de412f1f6b0aa59
Pietro-Guedes <pietro.g.oliveira6@aluno.senai.br>
2025-04-08 08:38:53 -0300

    Cálculo_projeto.py
    Cálculo de valores

--- Projeto_pietro1/Multa_projeto.py
+++ Projeto_pietro1/Multa_projeto.py
@@ -1,7 +1,21 @@
-#-*- coding: UTF-8 -*-
-print('Diga a veocidade do seu carro, que eu direi ce foi multado')
-car = float(input('Digita a velocidade: '))
-if car > 80:
-    vel= (car-80)*5
-    print('Você foi multado, o valor da multa é: ', vel)
-els
+print('Olá, peça para fazer um cálculo de adição, subtração, multiplicação ou divisão que irie fazer')
+num = int(input('Digite o número: '))
+num2 = int(input('Digite o segundo número: '))
+var = input('Digite qual operação usar: ')
+def operacao():
+    global num, num2, var
+    if var == '+':
+        num3 = num + num2
+        
+    elif var == '-':
+        num4 = num - num2
+        
+    elif var == '*':
+        num5 = num * num2
+        
+    elif var == '/':
+        num6 = num / num2
+        
+    else:
+       
+operacao()
